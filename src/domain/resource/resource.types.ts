5b0e139bf4878ac62105586d4fac40e6197a8180
Frank Weijers <frank@weijers.io>
2022-07-05 23:15:30 +0200

    Include resource config

--- src/domain/resource/resource.types.ts
+++ src/domain/resource/resource.types.ts
@@ -2,9 +2,6 @@ import { DateVO, UUID } from '@appvise/domain';
 import { File } from '../../domain';
 
 // Import ResourceParentType from config to allow each implementing application to define their own types
-// TODO: Find better way to allow for importing types from project
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { ResourceParentType } from '@config/appvise/resource';
 
 export { ResourceParentType };
