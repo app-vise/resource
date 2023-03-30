01396778b9c1ec702feeb7b9c98f8e12344504d8
Jay Doerga <jaydoerga@MacBook-Pro-van-Jay.local>
2023-03-30 15:05:23 +0200

    Ts fix

--- src/application/command/index.ts
+++ src/application/command/index.ts
@@ -1,10 +1,5 @@
 import { CreateResourceHandler } from './create-resource.handler';
-import { DeleteResourceHandler } from './delete-resource.handler';
 
 export { CreateResourceCommand } from './dto/create-resource.command';
-export { DeleteResourceCommand } from './dto/delete-resource.command';
 
-export const ResourceCommandHandlers = [
-  CreateResourceHandler,
-  DeleteResourceHandler,
-];
+export const ResourceCommandHandlers = [CreateResourceHandler];
